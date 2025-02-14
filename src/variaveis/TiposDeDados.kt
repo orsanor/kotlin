e402c1c74226d3f519feb98c13326dfc32052d60
Orsano <orsanor@gmail.com>
2025-02-13 22:32:39 -0300

    Tipos de dados

---
+++ src/variaveis/TiposDeDados.kt
@@ -0,0 +1,48 @@
+package variaveis
+
+fun main() {
+    // Tipos numéricos
+    /*
+    * Byte - 8
+    * Short - 16
+    * Int - 32
+    * Long - 64
+    * Float - 32
+    * Double - 64
+    */
+
+    val byte: Byte = 8
+    val short: Short = 360
+    val int: Int = 1000
+    val long: Long = 10000L
+    val float: Float = 150.10F
+    val double: Double = 300.10
+
+
+    println(byte)
+    println(short)
+    println(int)
+    println(long)
+    println(float)
+    println(double)
+
+    // Tipo Boolean
+    // true or false
+
+    val boolean: Boolean = true
+
+    println(boolean)
+
+    // Tipos textuais
+    /*
+    * Char
+    * String
+    * */
+
+
+    val char: Char = 'a'
+    val string: String = "Hello World"
+
+    println(string)
+    println(char)
+}
--- src/variaveis/Variaveis.kt
+++ src/variaveis/Variaveis.kt
@@ -1,2 +1,9 @@
 package variaveis
 
+fun main() {
+    var name: String = "Jonathan"
+    val lastname: String = "Orsano"
+
+    println(name)
+    println(lastname)
+}
